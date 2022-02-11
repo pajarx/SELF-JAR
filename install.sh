7979b94e8238dbc5dac14ce743a9f82d26361658
pajarx <97197623+pajarx@users.noreply.github.com>
2022-02-11 22:27:49 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs -y
+apt-get install libwebp -y
+apt-get install mc -y
+apt-get install ffmpeg -y
+apt-get install wget -y
+apt-get install tesseract -y
+apt-get install nmap -y
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "DONE INSTALLING ALL PACKAGE REQUIRE, TINGGAL RUN npm start"
